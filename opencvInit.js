e2e134cdbd89800f86d22e98f8fd2f114de5c6ec
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2024-02-05 10:04:51 +0900

    feat: image processing in js

---
+++ opencvInit.js
@@ -0,0 +1,4 @@
+let imgElement = document.getElementById('imageSrc');
+let inputElement = document.getElementById('fileInput');
+let canvasOutputOriginal = document.getElementById('canvasOutputOriginal');
+let canvasOutputProcessed = document.getElementById('canvasOutputProcessed');
