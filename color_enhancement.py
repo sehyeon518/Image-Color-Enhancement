87e1cbbcff69ca5def84feb1251d2e5edf3c6f17
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2024-02-05 14:25:12 +0900

    delete: js 삭제

--- color_enhancement.py
+++ color_enhancement.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import os
 from PIL import Image
 
 
@@ -20,16 +19,12 @@ def resize_image(img):
 
 
 def image_agcwd(img, a=0.25, truncated_cdf=False, normalize=1.5):
-    h, w = img.shape[:2]
     hist, bins = np.histogram(img.flatten(), 256, [0, 256])  # Y 채널에 대한 hist
-    cdf = hist.cumsum()
 
-    cdf_normalized = cdf / cdf.max()
-    prob_normalized = hist / hist.sum() # Y 채널에 대한 hist 총합 1
+    prob_normalized = hist / hist.sum()  # Y 채널에 대한 hist 총합 1
 
     unique_intensity = np.unique(img)
-    intensity_max = unique_intensity.max()
-    intensity_min = unique_intensity.min()
+
     prob_min = prob_normalized.min()
     prob_max = prob_normalized.max()
 
@@ -40,7 +35,7 @@ def image_agcwd(img, a=0.25, truncated_cdf=False, normalize=1.5):
     cdf_prob_normalized_wd = prob_normalized_wd.cumsum()
 
     if truncated_cdf:
-        inverse_cdf = np.maximum(normalize/2, normalize - cdf_prob_normalized_wd)
+        inverse_cdf = np.maximum(normalize / 2, normalize - cdf_prob_normalized_wd)
     else:
         inverse_cdf = normalize - cdf_prob_normalized_wd
 
@@ -51,14 +46,14 @@ def image_agcwd(img, a=0.25, truncated_cdf=False, normalize=1.5):
     return img_new
 
 
-def process_bright(img): # 밝은 이미지를 어둡게
+def process_bright(img):  # 밝은 이미지를 어둡게
     img_negative = 255 - img
     agcwd = image_agcwd(img_negative, a=0.25, truncated_cdf=False)
     reversed_agcwd = 255 - agcwd
     return reversed_agcwd
 
 
-def process_dimmed(img): # 어두운 이미지를 밝게
+def process_dimmed(img):  # 어두운 이미지를 밝게
     agcwd = image_agcwd(img, a=0.75, truncated_cdf=True)
     return agcwd
 
@@ -73,7 +68,6 @@ def image_brightness(img):
     mean_in = np.sum(Y / (M * N))
     t = (mean_in - exp_in) / exp_in
 
-    # Process image for gamma correction
     img_output = None
     if t < -threshold:  # 어두운 이미지
         print("Dimmed Image")
@@ -92,7 +86,7 @@ def image_brightness(img):
 
 def enhance_color(img, factor=1.2, count=1):
     dst = img
-    
+
     for _ in range(count):
         max_rgb = np.max(dst, axis=-1, keepdims=True)
         max_rgb_nonzero = np.where(max_rgb == 0, 1, max_rgb)
@@ -103,25 +97,27 @@ def enhance_color(img, factor=1.2, count=1):
     return dst.astype(np.uint8)
 
 
-def main(image):
+def color_enhancement(image):
     src = np.array(image)
     src = resize_image(src)
     cv2.imshow("src", src)
 
     brightened = image_brightness(src)
     cv2.imshow("brightened", brightened)
-    
+
     enhanced = enhance_color(brightened, factor=1.7)
     cv2.imshow("enhanced", enhanced)
     return enhanced
 
 
 if __name__ == "__main__":
+    import os
+
     pwd = os.path.dirname(os.path.realpath(__file__))
     img_path = os.path.join(pwd, "Images", "cup.jpg")
     src = cv2.imread(img_path)
     pil_image = Image.fromarray(src)
-    
-    main(pil_image)
+
+    color_enhancement(pil_image)
     cv2.waitKey()
     cv2.destroyAllWindows()
--- opencvInit.js
+++
@@ -1,4 +0,0 @@
-let imgElement = document.getElementById('imageSrc');
-let inputElement = document.getElementById('fileInput');
-let canvasOutputOriginal = document.getElementById('canvasOutputOriginal');
-let canvasOutputProcessed = document.getElementById('canvasOutputProcessed');
